132d4c3a37c608ea7df0e75354caf97373340582
Sofiene Lassoued <lassoued.sofiene@fh-swf.de>
2024-07-10 17:07:19 +0200

    Update log file.py

--- agent/train.py
+++ agent/train.py
@@ -1,34 +1,38 @@
 import  os
-import jsspetri
-import gymnasium as gym
-from sb3_contrib import MaskablePPO
+import ptrl
 import time
+import gymnasium as gym
 from datetime import datetime
+from sb3_contrib import MaskablePPO
 
 
-#tensorboard_log="logs"
-
-
-def train_jssp(instance_id, benchmark = 'Taillard', trans = True, trans_layout = None, timesteps=100000,dynamic=False):
-    env = gym.make("jsspetri-fms-v0",
-                   render_mode="solution",
+def train_jssp(instance_id,layout=1,n_agv=1 ,n_tt=0,timesteps=100000,dynamic=False,size=(None,None),render_mode="solution"):
+    env = gym.make("ptrl-fms-v0",
+                   render_mode=render_mode,
                    instance_id=instance_id,
-                   benchmark=benchmark,
-                   trans_layout = trans_layout,
-                   trans= trans,
+                   layout=layout,
+                   n_agv=n_agv,
+                   n_tt=n_tt,
                    dynamic=dynamic,
+                   size=size,
     ).unwrapped
     
+    current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M")
     
-    model = MaskablePPO("MlpPolicy", env, verbose=1,seed=101,tensorboard_log="logs")
+    log_file = f"logs/Log_{instance_id}_{layout}_{n_agv}_{n_tt}_{timesteps}_{current_datetime}.zip"
+    model = MaskablePPO("MlpPolicy", env, 
+                        ent_coef=0.01,
+                        verbose=1,
+                        seed=101,
+                        tensorboard_log=log_file
+                        )
 
     start_time = time.time()  
     model.learn(total_timesteps=timesteps)
-    end_time = time.time()
+    end_time = time.time()  
     elapsed_time = end_time - start_time  
-    current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M")
-    
-    
+  
+        
     # Check if the 'info' file exists
     if os.path.exists('0-info.txt'):
         with open('0-info.txt', 'a') as info_file:
@@ -38,26 +42,21 @@ def train_jssp(instance_id, benchmark = 'Taillard', trans = True, trans_layout =
             info_file.write(f" {current_datetime} -The total {instance_id} training time (seconds): {elapsed_time}\n")
       
     print(f"Training took {elapsed_time} seconds")
-    model.save(f"agents/MaskablePPO-{instance_id}-{timesteps}.zip")
+    model.save(f"agents/Agent_{instance_id}_{layout}_{n_agv}_{n_tt}_{timesteps}.zip")
 
 def main():
-
-    #instances= ["ta11","ta21","ta61","ta01"]
-    # instances= ["ta01"]
-    # timesteps = 100000
-    # benchmark = "Taillard"
-    #
-    # for instance_id  in instances :
-    #     train_jssp(instance_id, benchmark=benchmark, trans_layout='trans_15', timesteps=timesteps)
-    instances = ["bu01"]
-    timesteps = 100000
-    benchmark = "BU"
-    trans = True
-    trans_lay = 'trans_4_1'
-
-    for instance_id in instances:
-        train_jssp(instance_id, benchmark=benchmark, trans = trans, trans_layout=trans_lay, timesteps=timesteps)
-
+    
+    instances= ["ra01","ra02","ra03","ra04","ra05","ra06","ra07","ra08","ra09","ra10"]
+    layout=1
+    n_agv=2
+    n_tt=0
+    
+    timesteps =3e5
+    dynamic,size=False,(10,5)
+    render_mode="solution"
+    
+    for instance_id  in  instances :
+        train_jssp(instance_id,layout,n_agv=n_agv ,n_tt=n_tt,timesteps=timesteps,dynamic=dynamic,size=size,render_mode=render_mode)
 
 if __name__ == "__main__":
     main()
